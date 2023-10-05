821f2c36b97e10befc12f9ec13438f3a32eaf6cb
Семён <xlimitan@yandex.ru>
2023-10-05 19:13:13 +0700

    Initial commit

---
+++ src/main/java/com/telegrambot/animailsshelter/AnimailsShelterApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambot.animailsshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimailsShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimailsShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegrambot/animailsshelter/AnimailsShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambot.animailsshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimailsShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
