3e2f7cfb8cfd308aa5206fd503e42debc19c3159
Svidinskiy <100801664+Svidinskiy@users.noreply.github.com>
2023-10-17 00:33:34 +0400

    Начальный интерфейс пользователя (Этап 0)

--- src/main/java/com/telegrambot/animailsshelter/config/BotConfig.java
+++ src/main/java/com/telegrambot/animailsshelter/config/BotConfig.java
@@ -1,6 +1,8 @@
 package com.telegrambot.animailsshelter.config;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,5 +14,22 @@ public class BotConfig {
 @Value("${bot.name}")
     String botName;
     @Value("${bot.key}")
+            @Getter @Setter
     String token;
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
--- src/main/java/com/telegrambot/animailsshelter/model/Amimals.java
+++ src/main/java/com/telegrambot/animailsshelter/model/Amimals.java
@@ -2,6 +2,6 @@ package com.telegrambot.animailsshelter.model;
 
 import javax.persistence.Entity;
 
-@Entity
-public class Amimals {
-}
+//@Entity
+//public class Amimals {
+//}
