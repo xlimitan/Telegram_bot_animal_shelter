1bfca68597adca7218e8d07c6611f858f9694e1e
Семён <xlimitan@yandex.ru>
2023-10-17 11:31:11 +0700

    Регистрация пользователя в БД

--- src/main/resources/liquibase/scripts/notification.sql
+++ src/main/resources/liquibase/scripts/notification.sql
@@ -2,11 +2,11 @@
 
 -- changeset golenko:1
 
-create table notification_task (
+create table users_data_table (
 id bigserial primary key,
 chat_id int8 not null,
-firstName text not null,
-lastName text not null,
-userName text not null,
-registered_at timestamp not null
+first_name text not null,
+last_name text not null,
+user_name text not null,
+registered_at bytea not null
 )
